52f0c0c452d460d35a144446fa17507c9afcaee1
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-08-22 19:15:19 +0100

    testing how to create, export and import components

---
+++ qaweek10/src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+        <h1>Hello Daniel</h1>
+        <p>{2 + 7}</p>
+        </div>
+    )}
+}
---
+++ qaweek10/src/Meal.js
@@ -0,0 +1,9 @@
+export const Meal = function(menu, price) {
+    const menu = menu;
+    const price = price;
+    return { rank, suit };
+};
+
+const mealOne = Meal("Pizza", 6);
+
+
